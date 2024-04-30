aa4184cc6698a0cfb84409170fa70ad36f142cb5
wisdomstar94 <wisdomstar94@gmail.com>
2024-04-30 16:31:55 +0900

    수정.

--- js/RtnLottieViewNativeComponent.ts
+++ js/RtnLottieViewNativeComponent.ts
@@ -1,5 +1,4 @@
-import type { ViewProps } from "ViewPropTypes";
-import type { HostComponent } from "react-native";
+import type { HostComponent, ViewProps } from "react-native";
 import codegenNativeComponent from "react-native/Libraries/Utilities/codegenNativeComponent";
 
 export interface NativeProps extends ViewProps {
